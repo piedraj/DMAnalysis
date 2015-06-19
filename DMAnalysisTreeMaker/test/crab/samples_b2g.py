8ced2491646ef85fee103c5e26fe695d43dcfe6c
Juan Ferrero <juan.garcia.ferrero@cern.ch>
2015-06-19 11:19:17 +0200

    all samples included

--- DMAnalysisTreeMaker/test/crab/samples_b2g.py
+++ DMAnalysisTreeMaker/test/crab/samples_b2g.py
@@ -1,2 +1,33 @@
-samples['TTJets']      = ['/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/decosa-TT_Phys14DR-PU20bx25_PHYS14_25_V1-v1_EDMNtuple_18May2015_v2-4fb54d969ee15e14416bf4390fbb44b0/USER']
-samples['ZJetsToNuNu'] = ['/ZJetsToNuNu_HT-100to200_Tune4C_13TeV-madgraph-tauola/dpinna-ZJetsToNuNu_Phys14DR-PU20bx25_PHYS14_25_V1-v1_EDMNtuple_June2-4fb54d969ee15e14416bf4390fbb44b0/USER']
+
+#-----   annapaola 18vi15   --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+samples['TTJets']                   = ['/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/decosa-TT_Phys14DR-PU20bx25_PHYS14_25_V1-v1_EDMNtuple_18May2015_v2-4fb54d969ee15e14416bf4390fbb44b0/USER'                   ]
+
+samples['DYJetsToLL']               = ['/DYJetsToLL_M-50_13TeV-madgraph-pythia8/dpinna-DYJetsToLL_M-50_Phys14DR-PU20bx25_PHYS14_25_V1-v1_EDMNtuple_June2-4fb54d969ee15e14416bf4390fbb44b0/USER'                              ]
+samples['DYJetsToLL100to200']       = ['/DYJetsToLL_M-50_HT-100to200_Tune4C_13TeV-madgraph-tauola/dpinna-DYJetsToLL_M-50_HT-100to200_Phys14DR-PU20bx25_PHYS14_25_V1-v1_EDMNtuple_June2-4fb54d969ee15e14416bf4390fbb44b0/USER']
+samples['DYJetsToLL200to400']       = ['/DYJetsToLL_M-50_HT-200to400_Tune4C_13TeV-madgraph-tauola/dpinna-DYJetsToLL_M-50_HT-200to400_Phys14DR-PU20bx25_PHYS14_25_V1-v1_EDMNtuple_June2-4fb54d969ee15e14416bf4390fbb44b0/USER']
+samples['DYJetsToLL400to600']       = ['/DYJetsToLL_M-50_HT-400to600_Tune4C_13TeV-madgraph-tauola/dpinna-DYJetsToLL_M-50_HT-400to600_Phys14DR-PU20bx25_PHYS14_25_V1-v1_EDMNtuple_June2-4fb54d969ee15e14416bf4390fbb44b0/USER']
+samples['DYJetsToLL600toInf']       = ['/DYJetsToLL_M-50_HT-600toInf_Tune4C_13TeV-madgraph-tauola/dpinna-DYJetsToLL_M-50_HT-600toInf_Phys14DR-PU20bx25_PHYS14_25_V1-v1_EDMNtuple_June2-4fb54d969ee15e14416bf4390fbb44b0/USER']
+
+samples['ZJetsToNuNu100to200']      = ['/ZJetsToNuNu_HT-100to200_Tune4C_13TeV-madgraph-tauola/dpinna-ZJetsToNuNu_Phys14DR-PU20bx25_PHYS14_25_V1-v1_EDMNtuple_June2-4fb54d969ee15e14416bf4390fbb44b0/USER'                    ]
+samples['ZJetsToNuNu200to400']      = ['/ZJetsToNuNu_HT-200to400_Tune4C_13TeV-madgraph-tauola/dpinna-ZJetsToNuNu_HT-200to400_Phys14DR-PU20bx25_PHYS14_25_V1-v1_EDMNtuple_June2-4fb54d969ee15e14416bf4390fbb44b0/USER'        ]
+samples['ZJetsToNuNu400to600']      = ['/ZJetsToNuNu_HT-400to600_Tune4C_13TeV-madgraph-tauola/dpinna-ZJetsToNuNu_HT-400to600_Phys14DR-PU20bx25_PHYS14_25_V1-v1_EDMNtuple_June2-4fb54d969ee15e14416bf4390fbb44b0/USER'        ]
+samples['ZJetsToNuNu600toInf']      = ['/ZJetsToNuNu_HT-600toInf_Tune4C_13TeV-madgraph-tauola/dpinna-ZJetsToNuNu_HT-600toInf_Phys14DR-PU20bx25_PHYS14_25_V1-v1_EDMNtuple_June2-4fb54d969ee15e14416bf4390fbb44b0/USER'        ]
+
+samples['WJetsToLNu100to200']       = ['/WJetsToLNu_HT-100to200_Tune4C_13TeV-madgraph-tauola/oiorio-WJets_HT_100_200_EDMNTUPLE-68c5d5b03983a702c54950500b47aca5/USER'                                                        ]
+samples['WJetsToLNu200to400']       = ['/WJetsToLNu_HT-200to400_Tune4C_13TeV-madgraph-tauola/oiorio-WJets_HT_200_400_EDMNTUPLE-68c5d5b03983a702c54950500b47aca5/USER'                                                        ]
+samples['WJetsToLNu400to600']       = ['/WJetsToLNu_HT-400to600_Tune4C_13TeV-madgraph-tauola/oiorio-WJets_HT_400_600_EDMNTUPLE-68c5d5b03983a702c54950500b47aca5/USER'                                                        ]
+samples['WJetsToLNu600toInf']       = ['/WJetsToLNu_HT-600toInf_Tune4C_13TeV-madgraph-tauola/oiorio-WJets_HT_600_Inf_EDMNTUPLE-68c5d5b03983a702c54950500b47aca5/USER'                                                        ]
+
+#/QCD_HT-100To250_13TeV-madgraph/dpinna-QCD100To250_Phys14DR-PU20bx25_PHYS14_25_V1-v1_EDMNtuple_May21-4fb54d969ee15e14416bf4390fbb44b0/USER
+#/QCD_HT-500To1000_13TeV-madgraph/dpinna-QCD500To1000_Phys14DR-PU20bx25_PHYS14_25_V1-v1_EDMNtuple_May21-4fb54d969ee15e14416bf4390fbb44b0/USER
+#/QCD_HT_1000ToInf_13TeV-madgraph/dpinna-QCD1000ToInf_Phys14DR-PU20bx25_PHYS14_25_V1-v1_EDMNtuple_May21-4fb54d969ee15e14416bf4390fbb44b0/USER
+#/QCD_HT_250To500_13TeV-madgraph/dpinna-QCD250To500_Phys14DR-PU20bx25_PHYS14_25_V1-v1_EDMNtuple_May21-4fb54d969ee15e14416bf4390fbb44b0/USER
+
+#-----   orso 18vi15   ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- 
+samples['T_tW-channel']             = ['/T_tW-channel-DR_Tune4C_13TeV-CSA14-powheg-tauola/oiorio-t_tW_EDMNTUPLE-262715883604fc3cb4ebab0edc892255/USER'                                                                       ]
+samples['Tbar_tW-channel']          = ['/Tbar_tW-channel-DR_Tune4C_13TeV-CSA14-powheg-tauola/oiorio-tbar_tW_EDMNTUPLE-f7098dc7f2c067aca21aaabab3790de8/USER'                                                                 ]
+samples['TToLeptons_t-channel']     = ['/TToLeptons_t-channel-CSA14_Tune4C_13TeV-aMCatNLO-tauola/oiorio-t-channel_EDMNTUPLE-e8b7adc990866ea737a1f72d3211b373/USER'                                                           ]
+samples['TBarToLeptons_t-channel']  = ['/TBarToLeptons_t-channel_Tune4C_CSA14_13TeV-aMCatNLO-tauola/oiorio-tbar_t-channel_EDMNTUPLE-e8b7adc990866ea737a1f72d3211b373/US'                                                     ]
+
+
